edc3a61be786bb5771a83169b298aeb9f6312ee7
unknown <kendrick004@gmail.com>
2021-03-29 00:58:46 +0800

    use the files model in route to properly get, delete, and post files with db

--- routes/files.js
+++ routes/files.js
@@ -4,42 +4,73 @@ const mw = require('../middlewares/headers');
 const Files = require('../lib/Files');
 const mimetype = require('mime-types');
 const {unlinkSync} = require('fs');
+const FilesModel = require('../models/Files');
+const {v4} = require('uuid');
 
 // eslint-disable-next-line max-len
-router.post('/', [mw.checkMultipart, new Files('disk').single('file')], (req, res)=>{
-  res.json({
-    publicKey: 'testkey',
-    privateKey: 'testkey',
-  });
+router.post('/', [mw.checkMultipart, new Files('disk').single('file')], async (req, res)=>{
+  const payload = {
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    publicKey: v4(),
+    privateKey: v4(),
+    file: req.file,
+    storage: 'disk',
+  };
+  const file = await FilesModel.create(payload);
+  if (file) {
+    res.json({
+      publicKey: payload.publicKey,
+      privateKey: payload.privateKey,
+    });
+  } else {
+    res.status(400).json({
+      msg: 'Failed to upload a file',
+    });
+  }
 });
 
-router.get('/:publicKey', (req, res) => {
-  // eslint-disable-next-line max-len
-  const file = path.join(process.env.BASE_DIR, process.env.UPLOAD_DIR, '3.pdf');
-  let mime = mimetype.lookup(file);
-  // set only the type if unknown
-  if (!mime) {
-    mime = 'application/octet-stream';
-    res.setHeader('Content-Type', mime);
-  }
+router.get('/:publicKey', async (req, res) => {
+  const file = await FilesModel.getFileByPublicKey(req.params.publicKey);
+  if (file) {
+    const filePath = path.join(process.env.BASE_DIR, process.env.UPLOAD_DIR, file.file.originalname);
+    let mime = mimetype.lookup(filePath);
 
-  res.sendFile(file, (err)=>{
-    if (err) {
+    // set only the type if unknown
+    if (!mime) {
+      mime = 'application/octet-stream';
+      res.setHeader('Content-Type', mime);
+    }
+
+    res.sendFile(filePath, (err)=>{
+      if (err) {
+        res.status(400).json({
+          msg: 'File not found',
+        });
+      }
+    });
+  } else {
+    res.status(400).json({
+      msg: 'File not found',
+    });
+  }
+});
+
+router.delete('/:privateKey', async (req, res) => {
+  const file = await FilesModel.getFileByPrivateKey(req.params.privateKey);
+  if (file) {
+    const filePath = path.join(process.env.BASE_DIR, process.env.UPLOAD_DIR, file.file.originalname);
+    try {
+      unlinkSync(filePath);
+      await FilesModel.updateStatus(0, file.id);
+      res.json({
+        msg: 'Success',
+      });
+    } catch (e) {
       res.status(400).json({
         msg: 'File not found',
       });
     }
-  });
-});
-
-router.delete('/:privateKey', (req, res) => {
-  const file = path.join(process.env.BASE_DIR, process.env.UPLOAD_DIR, '3.pdf');
-  try {
-    unlinkSync(file);
-    res.json({
-      msg: 'Success',
-    });
-  } catch (e) {
+  } else {
     res.status(400).json({
       msg: 'File not found',
     });
