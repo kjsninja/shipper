0c48d64b0fcee0e76069fd97fa32379e10c1f88e
unknown <kendrick004@gmail.com>
2021-03-28 23:11:31 +0800

    add simple test for DiskStorage in File class

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
+const assert = require('assert').strict;
 
 describe('it should return 404 status code in non-existing route', ()=>{
   it('GET /', ()=>{
@@ -27,5 +28,20 @@ describe('it should return 200 status code in existing route', ()=>{
 
   it('DELETE /files/:privateKey', ()=>{
     return request(app).delete('/files/MyPrivateKey').expect(200);
+  });
+});
+
+
+const Files = require('../lib/Files');
+const DiskStorage = require('../lib/Storage/DiskStorage');
+describe('Files Unit Tests', ()=>{
+  it('disk storage as default storage', ()=>{
+    const disk = new Files();
+    return assert.equal(disk.storage instanceof DiskStorage, true);
+  });
+
+  it('disk storage passed as "disk"', ()=>{
+    const disk = new Files('disk');
+    return assert.equal(disk.storage instanceof DiskStorage, true);
   });
 });
