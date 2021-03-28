79162a9a8cc54d5607e36d3982ad1a265591d72b
unknown <kendrick004@gmail.com>
2021-03-28 23:13:09 +0800

    add base dir, encode url

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+process.env.BASE_DIR = __dirname;
 
 const express = require('express');
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
 
 app.use('/', require('./routes'));
 
