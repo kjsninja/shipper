d814ec374dc63d32b6e6b5192e1bab846bd9bf6c
unknown <kendrick004@gmail.com>
2021-03-28 23:46:50 +0800

    add knex config

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 5432,
+    },
+    pool: {min: 2, max: 7},
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 5432,
+    },
+    pool: {min: 2, max: 7},
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
